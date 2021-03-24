f7ecbaced4e83ba4d08e3474a56b78855d464f11
monic <monicaleonor98@gmail.com>
2021-03-23 20:33:01 -0600

    fist commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(drink){
+    document.querySelector(".pepsi").src = drink;
+}
+
+function changeBgColor(color){
+    const sec = document.querySelector(".sec");
+    sec.style.background = color;
+}
